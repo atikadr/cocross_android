e3782d6bf79fff1f1da255182b2220659036b259
Thearith <sothearith.sreang@gmail.com>
2014-01-25 18:38:45 +0800

    facebook log in success first stage

--- src/com/cocross/Facebook.java
+++ src/com/cocross/Facebook.java
@@ -1,48 +1,40 @@
 package com.cocross;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
-import android.widget.TextView;
 
-import com.parse.LogInCallback;
-import com.parse.Parse;
-import com.parse.ParseException;
 import com.parse.ParseFacebookUtils;
 import com.parse.ParseUser;
 
-public class Facebook extends Activity {
+public class Facebook extends FragmentActivity {
 
 	ParseUser facebookUser;
-	
-	private static final String FACEBOOK_APP_ID = "252904941538409";
+
+	private Fragment facebookFragment;
+
 	private static final String PARSE_APPLICATION_ID = "UxxptgHpkSjVgl8z9QWv6T2EoiaOzsk7sT3RNjWS";
 	private static final String PARSE_CLIENT_KEY = "PIQIEM0IGMUPEmcTQ1IYqwQqp7pNWsp9Ug5XtZkG";
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.facebook);
 		
-		 Parse.initialize(this, PARSE_APPLICATION_ID, PARSE_CLIENT_KEY);
 		
-		ParseFacebookUtils.initialize(FACEBOOK_APP_ID);
-		
-		ParseFacebookUtils.logIn(this, new LogInCallback(){
 
-			@Override
-			public void done(ParseUser user, ParseException e) {
-				if(user == null) {
-					
-				} else if(user.isNew()) {
-					
-				} else {
-					
-				}
-			}
+		facebookFragment = getSupportFragmentManager().findFragmentById(
+				android.R.id.content);
+
+		if (facebookFragment == null) {
+			facebookFragment = new FacebookFragment();
+
+			getSupportFragmentManager().beginTransaction()
+					.add(android.R.id.content, facebookFragment).commit();
 			
-		});
+		}
+
 	}
 
 	@Override
