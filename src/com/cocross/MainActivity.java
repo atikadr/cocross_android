26a8559dcf4f5d765b990ed08e5167778b2dc362
Thearith <sothearith.sreang@gmail.com>
2014-01-26 05:05:35 +0800

    delete last code

--- src/com/cocross/MainActivity.java
+++ src/com/cocross/MainActivity.java
@@ -208,18 +208,5 @@ public class MainActivity extends Activity {
 	    inflater.inflate(R.menu.action_bar, menu);
 	    return super.onCreateOptionsMenu(menu);
 	}
-	
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-	    // Handle presses on the action bar items
-	    switch (item.getItemId()) {
-	        case R.id.action_search:
-	            return true;
-	        case R.id.action_settings:
-	            return true;
-	        default:
-	            return super.onOptionsItemSelected(item);
-	    }
-	}
 
 }
