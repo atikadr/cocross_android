0b29ecfecebb9cf35662a0542e4bad405af9e7da
Keang <skeang@gmail.com>
2014-01-25 17:39:53 +0800

    added new log activity

--- src/com/cocross/ActivityNewLog.java
+++ src/com/cocross/ActivityNewLog.java
@@ -1,5 +1,30 @@
 package com.cocross;
 
-public class ActivityNewLog {
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+/**
+ * Activity to start a new log
+ * @author kaka
+ *
+ */
+public class ActivityNewLog extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_new_log);
+	}
+	
+	public void startButtonClicked(View v){
+		Log.d("started", "clicked");
+	}
+	
+	public void keyInButtonClicked(View v){
+		
+	}
+	
 
 }
