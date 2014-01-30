f2b5605d8c9c8cd4c7d951e1582bce3e8f7ce550
Keang <skeang@gmail.com>
2014-01-30 10:47:13 +0800

    Fixed back button behaviour

--- src/com/cocross/ActivityCounter.java
+++ src/com/cocross/ActivityCounter.java
@@ -98,8 +98,7 @@ public class ActivityCounter extends FragmentActivity {
 			
 			@Override
 			public void onClick(DialogInterface dialog, int which) {
-				Intent intent = new Intent(ActivityCounter.this, WorkOutList.class);
-				startActivity(intent);
+				finish();
 			}
 		});
 		
